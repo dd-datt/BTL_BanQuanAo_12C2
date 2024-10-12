ce1afd6c2d11e27894910e42da3cbd4084303a41
D3307 <22111060566@hunre.edu.vn>
2024-10-13 01:14:18 +0700

    Update footer.php
    phần footer chưa thêm link img qrcode

---
+++ footer.php
@@ -0,0 +1,71 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fashion_store</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="base.css">
+    <link rel="stylesheet" href="main.css">
+    <script src="https://kit.fontawesome.com/95b5cd4c14.js" crossorigin="anonymous"></script>
+</head>
+
+<footer class="footer">
+    <div class="grid">
+        <div class="grid-row">
+            <!-- Cột 1: Chăm sóc khách hàng -->
+            <div class="grid_column-2-4">
+                <h3 class="footer_heading">
+                    Chăm sóc khách hàng
+                </h3>
+                <ul class="footer_list">
+                    <li class="footer-item">
+                        <a href="#" class="footer-item-link">Trung tâm trợ giúp</a>
+                    </li>
+                    <li class="footer-item">
+                        <a href="#" class="footer-item-link">Hướng dẫn mua hàng</a>
+                    </li>
+                    <li class="footer-item">
+                        <a href="#" class="footer-item-link">Thanh toán</a>
+                    </li>
+                    <li class="footer-item">
+                        <a href="#" class="footer-item-link">Vận chuyển</a>
+                    </li>
+                    <li class="footer-item">
+                        <a href="#" class="footer-item-link">Chăm sóc khách hàng</a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- Cột 2: Theo dõi -->
+            <div class="grid_column-2-4">
+                <h3 class="footer_heading">
+                    Theo dõi
+                </h3>
+                <ul class="footer_list">
+                    <li class="footer-item">
+                        <a href="#" class="footer-item-link">
+                            <i class="footer-item-icon fa-brands fa-facebook"></i>
+                            Facebook
+                        </a>
+                    </li>
+                    <li class="footer-item">
+                        <a href="#" class="footer-item-link">
+                            <i class="footer-item-icon fa-brands fa-instagram"></i>
+                            Instagram
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- Cột 3: Hình ảnh -->
+            <div class="grid_column-2-4">
+                <div class="footer_dowload">
+                    <img src="../image/QR.png" alt="QR Code" class="footer_dowload-qr">
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
